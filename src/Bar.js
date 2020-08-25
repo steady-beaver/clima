3248f8b875bb7a9f81920e0b3ea4027193ee5fa6
steady-beaver <ivaylo.y.petrov@hotmail.com>
2020-08-25 15:34:32 +0300

    Successful connection with the Weather API - JSON response

--- src/Bar.js
+++ src/Bar.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react';
 
 class Bar extends Component{
 
-    handleSubmit = (e) => {
+    state = {
+        API_KEY_NAME: "ccec765bf7b5d7e644d77172a0eadb7c"
+    }
+
+    handleSubmit = async (e) => {
         e.preventDefault()
-        console.log(e.target["city"].value)
+        console.log(this.state.API_KEY_NAME)
+        let cityName = e.target["city"].value
+        const res = await fetch(`http://api.openweathermap.org/data/2.5/find?q=${cityName}&units=metric&appid=${this.state.API_KEY_NAME}`)
+        //const res = await fetch(`http://bulk.openweathermap.org/sample/`)
+        const res2 = await res.json()
+        console.log(res2)
     }
 
     render(){
