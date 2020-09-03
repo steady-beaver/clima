da5d9a635e945e0b42191f3f3a188ecba7ed39f3
steady-beaver <ivaylo.y.petrov@hotmail.com>
2020-09-03 09:57:22 +0300

    Added prop-types module

--- src/CityPanel.js
+++ src/CityPanel.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 import React from 'react'
 
 const CityPanel = ({city, sky, temp}) => (
@@ -5,5 +6,11 @@ const CityPanel = ({city, sky, temp}) => (
         <span>Sky above {city} will be {sky} with temperature of {temp}</span>
     </div>
 )
+
+CityPanel.propTypes = {
+    city: PropTypes.string.isRequired,
+    sky: PropTypes.string.isRequired,
+    temp: PropTypes.number.isRequired
+}
 
 export default CityPanel
