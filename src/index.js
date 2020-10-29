faa588fee1ac3ac0c2a3de9295b0d979d601f2b0
steady-beaver <ivaylo.y.petrov@hotmail.com>
2020-10-29 16:32:30 +0200

    Remove unnesessery Redux Old files.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
 import "materialize-css/dist/css/materialize.min.css";
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import App from './App';
 import './index.css';
-import allReducers from './reducers';
 import * as serviceWorker from './serviceWorker';
 
 
-const store = createStore(
-  allReducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
-
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
